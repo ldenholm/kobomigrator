1a5e68b40e2f57a7073b8497bfbee5545f568bcf
lochie <disrute@gmail.com>
2025-03-25 16:46:28 +1100

    implement fs copy

--- kobomigrator/Main.cpp
+++ kobomigrator/Main.cpp
@@ -4,6 +4,7 @@
 #include <chrono>
 
 namespace fs = std::filesystem;
+namespace ch = std::chrono;
 
 struct File {
 	fs::path filepath;
@@ -12,34 +13,54 @@ struct File {
 };
 
 int main() {
+	auto start = ch::high_resolution_clock::now();
+
 	fs::path fpath;
-	std::unordered_map<fs::path, File> dir_map;
-	fpath = "Resources/KobDemo";
+	fs::path migration_path;
+	std::unordered_map<fs::path, File> file_map;
+	fpath = "Resources/KobDemo"; // replace this with user input
+	migration_path = "migration_dir";
 	
 	for (const auto& dirEntry : fs::recursive_directory_iterator(fpath))
 	{
-		// check if we have a file
+		// check if we have a candidate file
 		if (dirEntry.is_regular_file() && 
 			(dirEntry.path().extension() == ".mobi" 
 			|| dirEntry.path().extension() == ".azw3"
 			|| dirEntry.path().extension() == ".pdf"
 			|| dirEntry.path().extension() == ".epub")) {
 			// create stuct and add it to dir_map
-			//std::cout << dirEntry.path().stem() << "filename w/o extension" << std::endl;
-			//std::cout << dirEntry.path().extension() << std::endl;
-			//std::cout << dirEntry.path().filename() << "filename path component" << std::endl;
-			//std::cout << dirEntry.path().relative_path() << "root path" << std::endl;
 			File f = {
 				dirEntry.path().relative_path(),
 				dirEntry.path().filename(),
 				dirEntry.last_write_time()
 			};
 
-			dir_map.insert({ dirEntry.path().relative_path(), f });
+			file_map.insert({ dirEntry.path().relative_path(), f });
 		}
-		std::cout << dirEntry.is_regular_file() << std::endl;
-		std::cout << dirEntry.path() << std::endl;
 
-		std::cout << dir_map.size() << "size of dir map\n";
+		std::cout << file_map.size() << "size of dir map\n";
 	}
+
+	// Now we have the map constructed. Let's choose a copy zone and begin processing
+	// each item in the map.
+
+	 // Copy files into new directory in current dir
+	if (fs::is_directory(migration_path)) {
+		std::cout << "Migration directory already exists, please check and remove it." << std::endl;
+		return -1;
+	}
+	
+	fs::create_directory(migration_path);
+
+	// process our map of files
+	for (const auto& [k, v] : file_map) {
+		std::error_code ec;
+		fs::copy_file(v.filepath, migration_path/v.filename, ec);
+		std::cout << ec.message() << std::endl;
+	}
+
+	auto finish = ch::high_resolution_clock::now();
+	std::cout << "completed in " << ch::duration_cast<ch::seconds>(finish - start).count() << "seconds" << std::endl;
+	return 0;
 }
